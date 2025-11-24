3695e787b7f584740e2978cbccaf1d2aedd05a18
dhananjay-jadhav <jadhav9260@gmail.com>
2025-11-24 09:36:56 +0530

    Added class validator

--- apps/tasks-api/src/users/dtos/create-user.type.ts
+++ apps/tasks-api/src/users/dtos/create-user.type.ts
@@ -1,12 +1,12 @@
-
-
+import { IsEmail, IsNotEmpty, IsStrongPassword } from 'class-validator';
 
 export class CreateUserInput {
-
+  @IsNotEmpty()
   username: string;
 
+  @IsEmail()
   email: string;
 
+  @IsStrongPassword()
   password: string;
-
 }
--- apps/tasks-api/src/users/dtos/update-user.type.ts
+++ apps/tasks-api/src/users/dtos/update-user.type.ts
@@ -1,7 +1,13 @@
+import { IsEmail, IsNotEmpty, IsUUID } from 'class-validator';
+
 export class UpdateUserInput {
+  @IsUUID()
   id: string;
 
+  @IsNotEmpty()
   username: string;
 
+  @IsNotEmpty()
+  @IsEmail()
   email: string;
 }
